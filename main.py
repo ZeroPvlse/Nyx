d930ab09f0fa48e598983048dcc04bc6820bf7dd
mvstermind <zablockidawid1@gmail.com>
2025-02-06 12:38:41 +0100

    feat: building help feature, fixing random errors need to fix out range erros

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ from nyx.nyx import Nyx
 
 def main():
     nyx = Nyx()
+    nyx.add_arg(long="arg", short="a", description="add argument", required=True)
+    nyx.add_arg(long="desc", short="d", description="description", required=True)
+    nyx.config(
+        description="this is test tool", example_input="--arg test --desc 'sum text'"
+    )
+    nyx._print_help()
 
-    nyx.add_argument("--arg", short="-a", desc="add argument")
-    nyx.add_argument("--desc", short="-d", desc="description")
-
-    nyx.parse_args(namespace=nyx)
-    print(nyx.arg)
+    nyx.parse_args()
 
 
 if __name__ == "__main__":
     main()
+
+# features i'll need in a future
+# startup funciton
+# generate custom examples
+# generate custom help message
+# sum ui features
--- nyx/nyx.py
+++ nyx/nyx.py
@@ -1,16 +1,66 @@
+from collections.abc import Callable
 import sys
+import os
 
 
 class Nyx:
-    def __init__(self):
+    def __init__(self, starting_function: Callable | None = None):
+        """
+        Initialize argument parser.
+
+        params:
+        starting_function -> function that will runn at the beginning of the program
+                            it can't take any input nor return any value.
+                            default value is None.
+        """
         self._arguments = {}
         self._short_to_long = {}
+        if starting_function is not None:
+            self.init(starting_function)
 
-    def add_argument(self, long: str, short: str, desc: str):
-        self._arguments[long.lstrip("-")] = {"description": desc, "value": None}
-        self._short_to_long[short.lstrip("-")] = long.lstrip("-")
+        self._example_usage = ""
+        self._help_options = [{}]
+        self._program_description = ""
+
+    def add_arg(
+        self, long: str, short: str, description: str, required: bool = False
+    ) -> None:
+        """
+        Add arguments to run your cli app.
+
+        params:
+        long: str -> full parameter name.       example: --test, --run
+        short: str -> short parameter name.     example: -t, -r
+        desc: str -> parameter description.     example: "test currently running code"
+        required: bool -> is this parameter required to run.
+                 default value is False.
+
+        returns -> None
+        """
+        self._arguments[long] = {
+            "description": description,
+            "value": None,
+            "required": required,
+        }
+        self._short_to_long[short] = long
+        self._help_options.append(
+            {
+                "long": long,
+                "short": short,
+                "description": description,
+                "required": required,
+            }
+        )
 
     def parse_args(self, namespace=None):
+        """
+        Check sys.argv for arguments defined using add_arg.
+
+        params:
+        namespace -> class that will have args sigined as it's attributes.
+                    default value is None.
+                    if default is None, Nyx will be used as namespace object
+        """
         if namespace is None:
             namespace = self
 
@@ -19,12 +69,25 @@ class Nyx:
         for i, arg in enumerate(sys.argv[1:]):
             if arg.startswith("-"):
                 if arg.startswith("--"):
+                    if arg == "--help":
+                        self._print_help()
+                        sys.exit(0)
+
                     arg_name = arg.lstrip("-")
-                    arg_value = sys.argv[i + 2] if i + 1 < len(sys.argv) else None
+                    arg_value = (
+                        sys.argv[i + 2] if i + 1 < len(sys.argv) else self._print_help()
+                    )
+
                 elif arg.startswith("-") and len(arg) == 2:
+                    if arg == "-help":
+                        self._print_help()
+                        sys.exit(0)
+
                     short = arg.lstrip("-")
                     arg_name = self._short_to_long.get(short)
-                    arg_value = sys.argv[i + 2] if i + 1 < len(sys.argv) else None
+                    arg_value = (
+                        sys.argv[i + 2] if i + 1 < len(sys.argv) else self._print_help()
+                    )
 
                 if arg_name and arg_name in self._arguments:
                     if arg_value:
@@ -32,6 +95,59 @@ class Nyx:
                         setattr(namespace, arg_name, arg_value)
 
         return namespace
+
+    def config(self, description: str, example_input: str):
+        """
+        Specify program description with example usage. this will execute
+        when -h or --help is called
+
+        params:
+        description: str -> program description that will display at the
+                            very begginig of help window
+        example_input: str -> example program usage.
+                            inclue just parameters and example inputs, excluding filename
+                            example:
+                            '--test main.py --time True'
+                            filename that calls this function is included by default
+
+        """
+        try:
+            example_input = str(example_input)
+            self._example_usage = example_input
+        except ValueError:
+            print(
+                f"example program usage must be a string but got {type(example_input)}"
+            )
+
+        try:
+            description = str(description)
+            self._program_description = description
+        except ValueError:
+            print(f"program description must be a string but got {type(example_input)}")
+
+    def _print_help(self):
+        script_name = os.path.basename(sys.argv[0])
+        print(
+            f"""{self._program_description}
+
+Usage: {script_name} {self._example_usage}
+
+Options:"""
+        )
+        for i in self._help_options:
+            if i == {}:
+                pass
+            else:
+                print(
+                    f"\t--{i['long']},\t-{i['short']}\trequired: {i['required']}\t {i['description']}"
+                )
+
+    def init(self, func: Callable):
+        """
+        Display starting ascii art or literally anything else that should run at the beginning
+        passed in function doesn't take anything nor return anything
+        """
+        func()
 
     def __getattr__(self, name):
         if name in self._arguments:
