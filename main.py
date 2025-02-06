d847fe5f8abc4110fd043dadc4d0a5ca65d23de8
mvstermind <zablockidawid1@gmail.com>
2025-02-06 09:46:24 +0100

    feat: arguments work (?) gotta fix edge cases

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from nyx.nyx import Nyx
 def main():
     nyx = Nyx()
 
-    nyx.add_argument("--arg")
+    nyx.add_argument("--arg", short="-a", desc="add argument")
+    nyx.add_argument("--desc", short="-d", desc="description")
 
-    nyx.parse_args(namespace=Nyx)
+    nyx.parse_args(namespace=nyx)
+    print(nyx.arg)
 
 
 if __name__ == "__main__":
--- nyx/nyx.py
+++ nyx/nyx.py
@@ -4,33 +4,32 @@ import sys
 class Nyx:
     def __init__(self):
         self._arguments = {}
+        self._short_to_long = {}
 
     def add_argument(self, long: str, short: str, desc: str):
-        self._arguments[long.lstrip("-")] = None
+        self._arguments[long.lstrip("-")] = {"description": desc, "value": None}
+        self._short_to_long[short.lstrip("-")] = long.lstrip("-")
 
     def parse_args(self, namespace=None):
         if namespace is None:
             namespace = self
 
-        args = sys.argv[1:]
-        i = 0
+        arg_name = ""
+        arg_value = ""
+        for i, arg in enumerate(sys.argv[1:]):
+            if arg.startswith("-"):
+                if arg.startswith("--"):
+                    arg_name = arg.lstrip("-")
+                    arg_value = sys.argv[i + 2] if i + 1 < len(sys.argv) else None
+                elif arg.startswith("-") and len(arg) == 2:
+                    short = arg.lstrip("-")
+                    arg_name = self._short_to_long.get(short)
+                    arg_value = sys.argv[i + 2] if i + 1 < len(sys.argv) else None
 
-        while i < len(args):
-            arg = args[i]
-
-            if arg.startswith("--"):
-                arg_name = arg.lstrip("-")
-
-                if arg_name in self._arguments:
-                    value = True
-                    if i + 1 < len(args) and not args[i + 1].startswith("--"):
-                        value = args[i + 1]
-                        i += 1
-
-                    setattr(namespace, arg_name, value)
-                    self._arguments[arg_name] = value
-
-            i += 1
+                if arg_name and arg_name in self._arguments:
+                    if arg_value:
+                        self._arguments[arg_name]["value"] = arg_value
+                        setattr(namespace, arg_name, arg_value)
 
         return namespace
 
